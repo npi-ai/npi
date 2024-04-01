0ec68d8920289b823b9c3f581da1dd2a48022db1
wenfeng <sxian.wang@gmail.com>
2024-04-01 17:47:27 -0500

    test google caldendar creation

--- npi/app/google/calendar/calendar.py
+++ npi/app/google/calendar/calendar.py
@@ -1,5 +1,17 @@
+import os
+from httplib2 import Credentials
 from openai import OpenAI
 import json
+
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 
 client = OpenAI(api_key="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")
 
@@ -78,9 +90,72 @@ def run_conversation():
         )  # get a new response from the model where it can see the function response
         return second_response
     
-print(run_conversation())    
+# print(run_conversation())    
 
+# The file token.json stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
+def get_creds():
+    creds = None
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    return creds
+ 
 
 def chat():
     print("Chatting with Google Calendar")
+
+def create_event():
+    print("Creating an event in Google Calendar")
+    # Refer to the Python quickstart on how to setup the environment:
+    # https://developers.google.com/calendar/quickstart/python
+    # Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+    # stored credentials.
+    service = build("calendar", "v3", credentials=get_creds())
+    event = {
+        'summary': 'Google I/O 2024',
+        'location': '800 Howard St., San Francisco, CA 94103',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+            'dateTime': '2024-04-03T09:00:00-07:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'end': {
+            'dateTime': '2024-04-03T17:00:00-07:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=2'
+        ],
+        'attendees': [
+            {'email': 'lpage@example.com'},
+            {'email': 'sbrin@example.com'},
+        ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+            {'method': 'email', 'minutes': 24 * 60},
+            {'method': 'popup', 'minutes': 10},
+            ],
+        },
+    }
+
+    event = service.events().insert(calendarId='ww@lifecycle.sh', body=event).execute()
+    print('Event created: %s' % (event.get('htmlLink')))
+
+create_event()
