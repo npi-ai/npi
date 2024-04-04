543ee0ec654d2f20567d49f8ed65599e967397f7
Daofeng Wu <dolphin.w.e@gmail.com>
2024-04-04 19:36:41 +0900

    refactor(gmail/params): use a more compact json schema

--- npi/app/google/gmail/shared/parameter.py
+++ npi/app/google/gmail/shared/parameter.py
@@ -11,4 +11,16 @@ class Parameter(BaseModel):
         for prop in schema.get('properties', {}).values():
             prop.pop('title', None)
 
+            # use a more compact format for optional fields
+            if 'anyOf' in prop and len(prop['anyOf']) == 2 and prop['anyOf'][1]['type'] == 'null':
+                # copy the first type definition to props
+                t = prop['anyOf'][0]
+                for k, v in t.items():
+                    prop[k] = v
+
+                prop.pop('anyOf', None)
+
+                if prop.get('default') is None:
+                    prop.pop('default', None)
+
         return schema
