468c7e14494f4d5a06a064f01ce2b0839679c339
wenfeng <sxian.wang@gmail.com>
2024-04-08 17:49:48 -0500

    feat: add fastapi

---
+++ npi/server/app.py
@@ -0,0 +1,22 @@
+"""the http server for NPI backend"""
+
+
+import uvicorn
+from fastapi import FastAPI
+from npi.server.router import router
+from npi.server.middleware import auth
+
+
+app = FastAPI()
+app.middleware("http")(auth)
+app.include_router(router)
+
+
+def main():
+    """the main function"""
+    uvicorn.run("npi.server.app:app",
+                host="0.0.0.0", port=8000, reload=True)
+
+
+if __name__ == "__main__":
+    main()
