2a8fa806eb2f9b19d861862f67ae18baea53fc88
wenfeng <sxian.wang@gmail.com>
2024-04-08 17:49:48 -0500

    feat: add fastapi

---
+++ npi/server/middleware.py
@@ -0,0 +1,12 @@
+"""the http server for NPI backend"""
+import time
+
+from fastapi import Request
+
+
+async def auth(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
--- npi/server/router.py
+++ npi/server/router.py
@@ -1,1 +1,18 @@
 """the http server for NPI backend"""
+
+from pydantic import BaseModel
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+class ChatRequest(BaseModel):
+    """the payload for chat request"""
+    app: str
+    task: str
+
+
+@router.post("/chat")
+async def chat(req: ChatRequest):
+    """the core api of NPI"""
+    return req
