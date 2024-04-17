d09b5d01b49dd9cd8a402435bb70ed15127a820d
Daofeng Wu <dolphin.w.e@gmail.com>
2024-04-18 06:52:09 +0900

    test(tweet/load_more_tweets): tweak prompt

--- npi/browser_app/twitter/__test__/load_more_tweets.spec.py
+++ npi/browser_app/twitter/__test__/load_more_tweets.spec.py
@@ -5,7 +5,7 @@ from npi.browser_app.twitter import Twitter
 async def main():
     twitter = Twitter(headless=False)
     await twitter.chat(
-        'Find the latest 10 tweets by @AtomSilverman. You should also include the link to the tweet. You must return exact 10 tweets.'
+        'Find the latest 10 tweets by @AtomSilverman. You should also include the link to the tweet. You must try to load more tweets until exactly 10 tweets are found. Output all tweets in your final response.'
     )
 
 
