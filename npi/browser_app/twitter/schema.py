dc3ea559576a9821ee3a0ea25858af9ee8063835
Daofeng Wu <dolphin.w.e@gmail.com>
2024-05-02 12:42:44 +0900

    feat(twitter): add action presets

--- npi/browser_app/twitter/schema.py
+++ npi/browser_app/twitter/schema.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from pydantic import Field
 from npi.types import Parameters
 
@@ -8,3 +9,19 @@ class GetTweetsParameters(Parameters):
 
 class OpenTweetParameters(Parameters):
     url: str = Field(description='URL of the tweet you want to open.')
+
+
+class SearchParameters(Parameters):
+    query: str = Field(description='Twitter search query')
+    sort_by: Literal['hottest', 'latest'] = Field(
+        default='hottest', description='Sort results by this parameter. Default is "hottest"'
+    )
+
+
+class GotoProfileParameters(Parameters):
+    username: str = Field(description='Twitter username')
+
+
+class ReplyParameters(Parameters):
+    url: str = Field(description='URL of the tweet you want to reply')
+    content: str = Field(description='Reply content')
