d4152fbbb161bf9951d3ba151dd1ec4d7b0cec2a
wenfeng <sxian.wang@gmail.com>
2024-05-15 11:33:42 -0500

    fix hitl

--- sdk/python/npiai/core/toolset.py
+++ sdk/python/npiai/core/toolset.py
@@ -10,17 +10,19 @@ from typing import List
 class ToolSet:
 
     def __init__(self, tools: List[App], llm: Client, hitl_handler: HITLHandler = None, ):
-        if len(tools) == 0:
-            raise Exception("At least one tool is required.")
-        self._tools = {}
-        for tool in tools:
-            tool.authorize()
-            self._tools[tool.tool_name()] = tool
         self._llm = llm
-
         self._hitl_handler = hitl_handler
         if self._hitl_handler is None:
             self._hitl_handler = EmptyHandler()
+
+        if len(tools) == 0:
+            raise Exception("At least one tool is required.")
+
+        self._tools = {}
+        for tool in tools:
+            tool.authorize()
+            tool.hitl(self._hitl_handler)
+            self._tools[tool.tool_name()] = tool
 
     def openai(self):
         tools = []
