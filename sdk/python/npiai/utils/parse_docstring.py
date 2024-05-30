ca98a18737847ed8df9f0d777814a3e49a3b2b7e
Daofeng Wu <dolphin.w.e@gmail.com>
2024-05-30 13:25:52 +0900

    refactor(npi): use FewShots in docstring

--- sdk/python/npiai/utils/parse_docstring.py
+++ sdk/python/npiai/utils/parse_docstring.py
@@ -2,12 +2,7 @@ from docstring_parser.google import GoogleParser, DEFAULT_SECTIONS, Section, Sec
 
 NPI_SECTIONS = [
     *DEFAULT_SECTIONS,
-    Section('Few Shots', 'few_shots', SectionType.SINGULAR),
-    Section('Few shots', 'few_shots', SectionType.SINGULAR),
-    Section('few shots', 'few_shots', SectionType.SINGULAR),
-    Section('Few Shot', 'few_shots', SectionType.SINGULAR),
-    Section('Few shot', 'few_shots', SectionType.SINGULAR),
-    Section('few shot', 'few_shots', SectionType.SINGULAR),
+    Section('FewShots', 'few_shots', SectionType.SINGULAR),
 ]
 
 
