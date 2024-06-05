55a5046007d1d2a4976683754d042b4505903927
Daofeng Wu <dolphin.w.e@gmail.com>
2024-06-05 21:10:48 +0900

    fix(app/add): only add prefix to apps

--- examples/quickstart/agent_mode.py
+++ examples/quickstart/agent_mode.py
@@ -1,13 +1,25 @@
 import asyncio
 
-from npiai.core import create_agent
+from npiai.core import App, create_agent
 from npiai.app.google.gmail import Gmail
 from examples.utils import load_gmail_credentials
 
 
+class MyApp(App):
+    def __init__(self):
+        super().__init__(
+            name='my_app',
+            description='test app',
+        )
+
+        self.add(
+            create_agent(Gmail(credentials=load_gmail_credentials()))
+        )
+
+
 async def main():
-    async with create_agent(Gmail(credentials=load_gmail_credentials())) as gmail:
-        print(await gmail.chat('get latest email in the inbox'))
+    async with create_agent(MyApp()) as agent:
+        print(await agent.chat('get latest email in the inbox'))
 
 
 if __name__ == "__main__":
