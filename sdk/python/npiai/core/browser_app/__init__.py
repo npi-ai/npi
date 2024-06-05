c1e571ce772f9a71ad8aefa9ee33383ac484e9bc
Daofeng Wu <dolphin.w.e@gmail.com>
2024-06-05 21:43:19 +0900

    refactor(navigator): agent only

--- sdk/python/npiai/core/browser_app/__init__.py
+++ sdk/python/npiai/core/browser_app/__init__.py
@@ -1,6 +1,6 @@
 from .browser_app import BrowserApp
 from .browser_agent import BrowserAgent
 from .playwright_context import PlaywrightContext
-from .navigator import Navigator, NavigatorAgent
+from .navigator_agent import NavigatorAgent
 
-__all__ = ['BrowserApp', 'BrowserAgent', 'PlaywrightContext', 'Navigator', 'NavigatorAgent']
+__all__ = ['BrowserApp', 'BrowserAgent', 'PlaywrightContext', 'NavigatorAgent']
