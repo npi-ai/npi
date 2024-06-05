917e4138bb0e5f22fa35478d6ebc7a28b5a170e1
Daofeng Wu <dolphin.w.e@gmail.com>
2024-06-05 21:01:53 +0900

    feat(app): add debug method

--- examples/quickstart/function_mode.py
+++ examples/quickstart/function_mode.py
@@ -3,12 +3,59 @@ import os
 
 from openai import OpenAI
 
+from npiai.core import App, npi_tool
 from npiai.app.google.gmail import Gmail
 from examples.utils import load_gmail_credentials
 
 
+class MyApp(App):
+    def __init__(self):
+        super().__init__(
+            name='my_app',
+            description='test app',
+        )
+
+        self.add(Gmail(credentials=load_gmail_credentials()))
+
+    @npi_tool
+    def echo(self, v: str):
+        """
+        Sync echo function.
+
+        Args:
+            v: input string
+        """
+        return v
+
+    @npi_tool
+    async def async_echo(self, v: str = 'Hello Async!'):
+        """
+        Async echo function.
+
+        Args:
+            v: input string
+        """
+        return v
+
+
 async def main():
-    async with Gmail(credentials=load_gmail_credentials()) as gmail:
+    async with MyApp() as my_app:
+        # debug fn
+        print(
+            'Debug:echo:',
+            await my_app.debug(
+                fn_name='echo',
+                args={'v': 'Hello World!'}
+            )
+        )
+
+        print(
+            'Debug:async_echo:',
+            await my_app.debug(
+                fn_name='async_echo',
+            )
+        )
+
         client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
         messages = [
             {
@@ -19,7 +66,7 @@ async def main():
         response = client.chat.completions.create(
             model="gpt-4o",
             messages=messages,
-            tools=gmail.tools,  # use npi as a tool package
+            tools=my_app.tools,  # use npi as a tool package
             tool_choice="auto",
             max_tokens=4096,
         )
@@ -27,7 +74,7 @@ async def main():
 
         messages.append(response_message)
 
-        print(await gmail.call(response_message.tool_calls))
+        print(await my_app.call(response_message.tool_calls))
 
 
 if __name__ == "__main__":
