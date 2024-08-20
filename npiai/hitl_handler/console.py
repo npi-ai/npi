b1f17b576eab576b87be69b54d45a6f795f6849c
Daofeng Wu <dolphin.w.e@gmail.com>
2024-08-20 20:09:06 +0900

    feat(hitl): add default values

--- npiai/hitl_handler/console.py
+++ npiai/hitl_handler/console.py
@@ -6,15 +6,32 @@ from npiai.context import Context
 
 
 class ConsoleHandler(HITL):
-    async def confirm(self, ctx: Context, tool_name: str, message: str) -> bool:
+    async def confirm(
+        self,
+        ctx: Context,
+        tool_name: str,
+        message: str,
+        default=False,
+    ) -> bool:
         # print(colored(f'[{tool_name}]: Confirmation required', color='green', attrs=['bold']))
-        msg = colored(message, color="magenta", attrs=["bold"])
         return await questionary.confirm(
-            f"[{tool_name}]: Please confirm: {msg}",
-            default=False,
+            colored(
+                f"[{tool_name}]: Please confirm: {message}",
+                color="magenta",
+                attrs=["bold"],
+            ),
+            default=default,
         ).ask_async()
 
-    async def input(self, ctx: Context, tool_name: str, message: str) -> str:
+    async def input(
+        self,
+        ctx: Context,
+        tool_name: str,
+        message: str,
+        default="",
+    ) -> str:
         # print(colored(f'[{tool_name}]: Additional information required', color='green', attrs=['bold']))
         print(colored(f"[{tool_name}]: {message}", color="magenta", attrs=["bold"]))
-        return await questionary.text("Type your response:").ask_async()
+        return await questionary.text(
+            "Type your response:", default=default
+        ).ask_async()
