a4b6acbc5676a8b2a6653c3fb0de3a748e86df42
Daofeng Wu <dolphin.w.e@gmail.com>
2024-10-16 12:04:32 +0900

    refactor(scraper): support structured column definitions

--- npiai/tools/web/scraper/__test__/column_inference.py
+++ npiai/tools/web/scraper/__test__/column_inference.py
@@ -1,25 +1,32 @@
 import asyncio
+import json
+
 from npiai.tools.web.scraper import Scraper
 from npiai.utils.test_utils import DebugContext
+
+url = "https://www.bardeen.ai/playbooks"
+ancestor_selector = ".playbook_list"
+items_selector = ".playbook_list .playbook_item-link"
 
 
 async def main():
     async with Scraper(headless=False, batch_size=10) as scraper:
         columns = await scraper.infer_columns(
             ctx=DebugContext(),
-            url="https://www.bardeen.ai/playbooks",
-            ancestor_selector=".playbook_list",
-            items_selector=".playbook_list .playbook_item-link",
+            url=url,
+            ancestor_selector=ancestor_selector,
+            items_selector=items_selector,
         )
 
-        print("Inferred columns:", columns)
+        print("Inferred columns:", json.dumps(columns, indent=2))
 
         await scraper.summarize(
             ctx=DebugContext(),
-            url="https://www.bardeen.ai/playbooks",
-            ancestor_selector=".playbook_list",
-            items_selector=".playbook_list .playbook_item-link",
+            url=url,
+            ancestor_selector=ancestor_selector,
+            items_selector=items_selector,
             output_columns=columns,
+            output_file=".cache/bardeen.json",
             limit=10,
         )
 
