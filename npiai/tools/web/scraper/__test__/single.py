59684a6e92336ce108d866777dabae3094915037
Daofeng Wu <dolphin.w.e@gmail.com>
2024-10-21 15:10:49 +0900

    refactor(scraper): optimize single entry column inference

--- npiai/tools/web/scraper/__test__/single.py
+++ npiai/tools/web/scraper/__test__/single.py
@@ -7,24 +7,17 @@ from npiai.utils.test_utils import DebugContext
 
 async def main():
     async with Scraper(headless=False, batch_size=10) as scraper:
+        columns = await scraper.infer_columns(
+            ctx=DebugContext(),
+            url="https://www.bardeen.ai/playbooks/get-data-from-the-currently-opened-imdb-com-title-page",
+        )
+        print("Inferred columns:", json.dumps(columns, indent=2))
+
         stream = scraper.summarize_stream(
             ctx=DebugContext(),
             url="https://www.bardeen.ai/playbooks/get-data-from-the-currently-opened-imdb-com-title-page",
             limit=1,
-            output_columns=[
-                {
-                    "name": "Title",
-                    "description": "Playbook title",
-                },
-                {
-                    "name": "Description",
-                    "description": "The description of the playbook",
-                },
-                {
-                    "name": "Steps to Run",
-                    "description": "The steps to run the playbook",
-                },
-            ],
+            output_columns=columns,
         )
 
         async for items in stream:
