267e0135a95ceb37628079386eda3df326ff752a
Daofeng Wu <dolphin.w.e@gmail.com>
2024-10-21 17:04:01 +0900

    feat(analyzer): support items_selector in infinite scroll detection

--- npiai/tools/web/scraper/__test__/interactive.py
+++ npiai/tools/web/scraper/__test__/interactive.py
@@ -1,79 +1,24 @@
 import asyncio
-import json
-from textwrap import indent
-
-from npiai.tools.web.scraper import Scraper
-from npiai.tools.web.page_analyzer import PageAnalyzer
 
 # from npiai.utils.test_utils import DebugContext
 from npiai import Context
+from npiai.tools.web.page_analyzer import PageAnalyzer
+from npiai.tools.web.scraper import Scraper
+from utils import autos_scrape
 
 
 async def main():
     url = input("Enter the URL: ")
-    ctx = Context()
 
     async with PageAnalyzer(headless=False) as analyzer:
         scraper = Scraper(batch_size=10, playwright=analyzer.playwright)
 
-        print(f"Analyzing {url}:")
-
-        infinite_scroll = await analyzer.support_infinite_scroll(
+        await autos_scrape(
+            ctx=Context(),
+            analyzer=analyzer,
+            scraper=scraper,
             url=url,
         )
-
-        print("  - Support infinite scroll:", infinite_scroll)
-
-        pagination = await analyzer.get_pagination_button(
-            ctx=ctx,
-            url=url,
-        )
-
-        print("  - Pagination button:", pagination)
-
-        scraping_type = await analyzer.infer_scraping_type(
-            ctx=ctx,
-            url=url,
-        )
-
-        print("  - Inferred scraping type:", scraping_type)
-
-        if scraping_type == "list-like":
-            selectors = await analyzer.infer_similar_items_selector(ctx, url)
-
-            print(
-                "  - Possible selectors:",
-                indent(json.dumps(selectors, indent=2), prefix="    ").lstrip(),
-            )
-
-        if not selectors:
-            return
-
-        columns = await scraper.infer_columns(
-            ctx=ctx,
-            url=url,
-            scraping_type="list-like",
-            ancestor_selector=selectors["ancestor"],
-            items_selector=selectors["items"],
-        )
-
-        print(
-            "  - Inferred columns:",
-            indent(json.dumps(columns, indent=2), prefix="   ").lstrip(),
-        )
-
-        stream = scraper.summarize_stream(
-            ctx=ctx,
-            url=url,
-            scraping_type="list-like",
-            ancestor_selector=selectors["ancestor"],
-            items_selector=selectors["items"],
-            output_columns=columns,
-            limit=10,
-        )
-
-        async for items in stream:
-            print("Chunk:", json.dumps(items, indent=2, ensure_ascii=False))
 
 
 if __name__ == "__main__":
---
+++ npiai/tools/web/scraper/__test__/utils.py
@@ -0,0 +1,80 @@
+import json
+from textwrap import indent
+
+from npiai.tools.web.scraper import Scraper
+from npiai.tools.web.page_analyzer import PageAnalyzer
+
+# from npiai.utils.test_utils import DebugContext
+from npiai import Context
+
+
+async def autos_scrape(
+    ctx: Context,
+    analyzer: PageAnalyzer,
+    scraper: Scraper,
+    url: str,
+):
+    ancestor_selector = None
+    items_selector = None
+
+    print(f"Analyzing {url}:")
+
+    scraping_type = await analyzer.infer_scraping_type(
+        ctx=ctx,
+        url=url,
+    )
+
+    print("  - Inferred scraping type:", scraping_type)
+
+    if scraping_type == "list-like":
+        selectors = await analyzer.infer_similar_items_selector(ctx, url)
+
+        print(
+            "  - Possible selectors:",
+            indent(json.dumps(selectors, indent=2), prefix="    ").lstrip(),
+        )
+
+        if selectors:
+            ancestor_selector = selectors["ancestor"]
+            items_selector = selectors["items"]
+
+    infinite_scroll = await analyzer.support_infinite_scroll(
+        url=url,
+        items_selector=items_selector,
+    )
+
+    print("  - Support infinite scroll:", infinite_scroll)
+
+    pagination_button_selector = await analyzer.get_pagination_button(
+        ctx=ctx,
+        url=url,
+    )
+
+    print("  - Pagination button:", pagination_button_selector)
+
+    columns = await scraper.infer_columns(
+        ctx=ctx,
+        url=url,
+        scraping_type=scraping_type,
+        ancestor_selector=ancestor_selector,
+        items_selector=items_selector,
+    )
+
+    print(
+        "  - Inferred columns:",
+        indent(json.dumps(columns, indent=2), prefix="   ").lstrip(),
+    )
+
+    stream = scraper.summarize_stream(
+        ctx=ctx,
+        url=url,
+        scraping_type=scraping_type,
+        ancestor_selector=ancestor_selector,
+        items_selector=items_selector,
+        pagination_button_selector=pagination_button_selector,
+        output_columns=columns,
+        limit=1 if scraping_type == "single" else 10,
+    )
+
+    async for items in stream:
+        print("Chunk:", json.dumps(items, indent=2, ensure_ascii=False))
